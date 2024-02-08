fecc8e26bf162e85a2600f9bfa7d3dd52ed9d7a3
Adrian Allen <adrianallen20@outlook.com>
2024-02-08 22:29:45 +0000

    swipe feature

--- src/components/Jam.jsx
+++ src/components/Jam.jsx
@@ -4,13 +4,33 @@ import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../contexts/authContext";
 import firebase from "../firebase";
 import logoImage from '../assets/jflogo.png'; 
+import { sculptureList } from './data.js';
+import { useState } from 'react';
 
 
 const Jam = () => {
   const { user } = useContext(AuthContext);
-
   const navigate = useNavigate();
+///swipe
+const [index, setIndex] = useState(0);
+const [showMore, setShowMore] = useState(false);
+let hasPrev = index > 0;
+let hasNext = index < sculptureList.length - 1;
+function handlePrevClick() {
+  if (hasPrev) {
+    setIndex(index - 1);
+  }
+}
 
+function handleNextClick() {
+  if (hasNext) {
+    setIndex(index + 1);
+  }
+}
+function handleMoreClick() {
+  setShowMore(!showMore);
+}
+let sculpture = sculptureList[index];
   const signOut = async () => {
     await firebase
       .auth()
@@ -22,6 +42,8 @@ const Jam = () => {
         console.log("err user signOut ", err);
       });
   };
+
+ 
 
   return (
     <>
@@ -49,6 +71,13 @@ const Jam = () => {
 
       <h1>Jam </h1>                      
       <p> Jam page Email: {user.email}</p>
+      <button onClick={handlePrevClick} disabled={!hasPrev}>Previous</button>
+      <button onClick={handleNextClick} disabled={!hasNext}>Next</button>
+      <h2><i>{sculpture.name}</i> by {sculpture.artist}</h2>
+      <h3>({index + 1} of {sculptureList.length})</h3>
+      <button onClick={handleMoreClick}>{showMore ? 'Hide' : 'Show'} details</button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
 
     </>
   );
